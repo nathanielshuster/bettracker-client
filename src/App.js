0295a1fbb9093e6b24c86125abe88a7c79dc523f
Nathaniel Shuster <shusternathaniel@gmail.com>
2020-06-22 22:36:51 -0400

    working on reroute after login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { Switch, Route, withRouter } from 'react-router-dom'
 
 import { Layout } from './components/Layout'
 import { NavigationBar } from './components/NavigationBar'
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Router } from 'react-router';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
